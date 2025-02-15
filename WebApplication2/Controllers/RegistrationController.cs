d6cdb932c3fc443976cbbd9890fb9bae58b9caf4
Aristique <arissmoonest@gmail.com>
2025-02-15 18:15:33 +0200

    Добавил сверху окно аутентификации

--- WebApplication2/Controllers/RegistrationController.cs
+++ WebApplication2/Controllers/RegistrationController.cs
@@ -6,17 +6,12 @@ using System.Web.Mvc;
 
 namespace WebApplication2.Controllers
 {
-    public class HomeController : Controller
+    public class RegistrationController : Controller
     {
-        // GET: Home
+        // GET: Registration
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult About()
-        {
-            return View();
-        }
     }
-
 }
