c99e07e5f2917241949fc39384a39d846253a24a
lyterar <sokotandrej96@gmail.com>
2025-03-20 19:02:12 +0200

    добавил контроллер для сериалов

---
+++ Bussines logic/Icomment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines_logic
+{
+    public interface Icomment
+    {
+        void Addcomment (string comment );
+        void Removecomment (int commentId);
+        void Modifycomment(int commentId,string comment);
+
+
+
+    }
+
+}
