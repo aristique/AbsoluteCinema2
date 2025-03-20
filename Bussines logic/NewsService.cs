305e51bc8c6906e56b9c324933098eae7c3db1fc
Aristique <arissmoonest@gmail.com>
2025-03-20 20:10:51 +0200

    Добавил изменения в новостях

--- Bussines logic/NewsService.cs
+++ Bussines logic/NewsService.cs
@@ -12,7 +12,7 @@ namespace Bussines_logic
 
         public NewsService(INewsRepository newsRepository)
         {
-            _newsRepository = newsRepository;
+            _newsRepository = newsRepository ?? throw new ArgumentNullException(nameof(newsRepository));
         }
 
         public async Task<IEnumerable<NewsDto>> GetAllNewsAsync()
@@ -20,14 +20,52 @@ namespace Bussines_logic
             return await _newsRepository.GetAllAsync();
         }
 
-        public async Task<NewsDto> GetNewsByIdAsync(int id)
+        public async Task<NewsDto> GetNewsByIdAsync(int newsId)
         {
-            return await _newsRepository.GetByIdAsync(id);
+            return await _newsRepository.GetByIdAsync(newsId);
         }
 
         public async Task<IEnumerable<NewsDto>> GetLatestNewsAsync(int count)
         {
             return await _newsRepository.GetLatestAsync(count);
+        }
+
+        // Реализация отсутствующих методов
+        public void AddNews(string title, string content)
+        {
+            var news = new NewsDto
+            {
+                Title = title,
+                Content = content,
+                PublishedAt = DateTime.UtcNow
+            };
+            _newsRepository.Add(news);
+        }
+
+        public void RemoveNews(int newsId)
+        {
+            _newsRepository.Remove(newsId);
+        }
+
+        public void ModifyNews(int newsId, string title, string content)
+        {
+            var news = _newsRepository.GetById(newsId);
+            if (news != null)
+            {
+                news.Title = title;
+                news.Content = content;
+                _newsRepository.Update(news);
+            }
+        }
+
+        public IEnumerable<NewsDto> GetAllNews()
+        {
+            return _newsRepository.GetAll();
+        }
+
+        public NewsDto GetNewsById(int newsId)
+        {
+            return _newsRepository.GetById(newsId);
         }
     }
 }
