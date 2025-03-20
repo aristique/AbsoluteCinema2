21b978324b1157c6caca16fab4cf049bae697bcf
Aristique <arissmoonest@gmail.com>
2025-03-20 19:53:14 +0200

    Добавил бизнес логику для новостей

---
+++ Bussines logic/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines_logic
+{
+    public interface INewsService
+    {
+        void AddNews(string title, string content);
+        void RemoveNews(int newsId);
+        void ModifyNews(int newsId, string title, string content);
+        IEnumerable<NewsDto> GetAllNews();
+        NewsDto GetNewsById(int newsId);
+    }
+}
---
+++ Bussines logic/NewsService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines_logic
+{
+    public class NewsService : INewsService
+    {
+        private readonly INewsRepository _newsRepository;
+
+        public NewsService(INewsRepository newsRepository)
+        {
+            _newsRepository = newsRepository;
+        }
+
+        public async Task<IEnumerable<NewsDto>> GetAllNewsAsync()
+        {
+            return await _newsRepository.GetAllAsync();
+        }
+
+        public async Task<NewsDto> GetNewsByIdAsync(int id)
+        {
+            return await _newsRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<NewsDto>> GetLatestNewsAsync(int count)
+        {
+            return await _newsRepository.GetLatestAsync(count);
+        }
+    }
+}
